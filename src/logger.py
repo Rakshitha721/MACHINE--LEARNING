4c2d366a7f4966f4fe5789a43f8c978c7597dde4
rakshithashekar <rakshithashekar72@gmail.com>
2025-08-02 12:27:37 +0530

    EDA

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,4 @@
-import logging 
+'''import logging 
 import os
 from datetime import datetime
 
@@ -18,3 +18,27 @@ logging.basicConfig(
 if __name__ =="__main__":
     logging.info("Logging has started")
 
+'''
+import logging 
+import os
+from datetime import datetime
+
+# Create only the folder 'logs', not the file
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Generate log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Final log file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Setup logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
